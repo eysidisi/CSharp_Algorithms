ad76eef454965fe242af28d8e78062b36969af8b
ali elmas <a.ihsan.elmas@gmail.com>
2022-01-11 21:51:56 +0300

    Start implementing counting inversions in an array

---
+++ Algorithms.Chapter1.Tests/Count/CountInversionsTests.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Algorithms.Chapter1.Count;
+
+namespace Algorithms.Chapter1.Tests.Count
+{
+    public class CountInversionsTests
+    {
+        [Fact]
+        public void NativeApproach_ArrayWithNoElement_ReturnsZero()
+        {
+            // Arrange
+            CountInversions countInversions = new CountInversions();
+            int[] input = new int[0];
+            int expectedOutput = 0;
+
+            // Act
+            int actualOutput = countInversions.NativeApproach(input);
+
+            // Assert
+            Assert.Equal(expectedOutput, actualOutput);
+        }
+
+        [Fact]
+        public void NativeApproach_ArrayWithOneElement_ReturnsZero()
+        {
+            // Arrange
+            CountInversions countInversions = new CountInversions();
+            int[] input = new int[1];
+            int expectedOutput = 0;
+
+            // Act
+            int actualOutput = countInversions.NativeApproach(input);
+
+            // Assert
+            Assert.Equal(expectedOutput, actualOutput);
+        }
+
+        [Fact]
+        public void NativeApproach_ArrayWithIncreasingElements_ReturnsZero()
+        {
+            // Arrange
+            CountInversions countInversions = new CountInversions();
+            int[] input = new int[] { 1, 2, 3, 4 };
+            int expectedOutput = 0;
+
+            // Act
+            int actualOutput = countInversions.NativeApproach(input);
+
+            // Assert
+            Assert.Equal(expectedOutput, actualOutput);
+        }
+
+        [Fact]
+        public void NativeApproach_ArrayWithDecreasingElements_ReturnsInversions()
+        {
+            // Arrange
+            CountInversions countInversions = new CountInversions();
+            int[] input = new int[] { 4, 3, 2, 1 };
+            int expectedOutput = 6;
+
+            // Act
+            int actualOutput = countInversions.NativeApproach(input);
+
+            // Assert
+            Assert.Equal(expectedOutput, actualOutput);
+        }
+
+        [Fact]
+        public void NativeApproach_ArrayWithIncreasingAndDecreasingElements_ReturnsInversions()
+        {
+            // Arrange
+            CountInversions countInversions = new CountInversions();
+            int[] input = new int[] { 5, 6, 3, 4 };
+            int expectedOutput = 4;
+
+            // Act
+            int actualOutput = countInversions.NativeApproach(input);
+
+            // Assert
+            Assert.Equal(expectedOutput, actualOutput);
+        }
+    }
+}
---
+++ Algorithms.Chapter1/Count/CountInversions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Chapter1.Count
+{
+    public class CountInversions
+    {
+        public int NativeApproach(int[] arr)
+        {
+            int numOfInversions = 0;
+
+            for (int firstIndex = 0; firstIndex < arr.Length - 1; firstIndex++)
+            {
+                for (int secondIndex = firstIndex + 1; secondIndex < arr.Length; secondIndex++)
+                {
+                    if (arr[firstIndex] > arr[secondIndex])
+                    {
+                        numOfInversions++;
+                    }
+                }
+            }
+
+            return numOfInversions;
+        }
+
+        public int DivideAndConquerApproach(int[] arr)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
