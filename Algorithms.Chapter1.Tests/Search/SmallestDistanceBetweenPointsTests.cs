450547a0af0c174629b6a0af44e0ae1247c70da5
ali elmas <a.ihsan.elmas@gmail.com>
2022-01-17 13:48:57 +0300

    Finish smallest distance between points

--- Algorithms.Chapter1.Tests/Search/SmallestDistanceBetweenPointsTests.cs
+++ Algorithms.Chapter1.Tests/Search/SmallestDistanceBetweenPointsTests.cs
@@ -82,20 +82,176 @@ namespace Algorithms.Part1.Tests.Search
         [Fact]
         public void FourPoints_MinDistInTheSplit()
         {
-
-            Assert.True(false);
             // Arrange
             Point[] points = new Point[]
             {
                 new Point(0, 0),
                 new Point(2, 4),
-                new Point(3, 6),
-                new Point(1, 3)
+                new Point(3, -4),
+                new Point(1, -3)
             };
 
             SmallestDistanceBetweenPoints smallestDistanceBetweenPoints = new SmallestDistanceBetweenPoints();
 
             int expectedOutput = 5;
+
+            // Act
+            int actualOutput = smallestDistanceBetweenPoints.FindSmallestDistanceBetweenPoints(points);
+
+            // Assert
+            Assert.Equal(expectedOutput, actualOutput);
+        }
+
+        [Fact]
+        public void EvenNumberOfPoints_MinDistInTheBeginning()
+        {
+            // Arrange
+            Point[] points = new Point[100];
+
+            for (int i = 0; i < points.Length; i++)
+            {
+                points[i] = new Point(i * 2, i * 2);
+            }
+
+            points[0].xCoordinate = points[0].xCoordinate + 1;
+            points[0].yCoordinate = points[0].yCoordinate + 1;
+
+            SmallestDistanceBetweenPoints smallestDistanceBetweenPoints = new SmallestDistanceBetweenPoints();
+
+            int expectedOutput = 2;
+
+            // Act
+            int actualOutput = smallestDistanceBetweenPoints.FindSmallestDistanceBetweenPoints(points);
+
+            // Assert
+            Assert.Equal(expectedOutput, actualOutput);
+        }
+
+        [Fact]
+        public void EvenNumberOfPoints_MinDistInTheSplit()
+        {
+            // Arrange
+            Point[] points = new Point[100];
+
+            for (int i = 0; i < points.Length; i++)
+            {
+                points[i] = new Point(i * 2, i * 2);
+            }
+
+            int midPoint = points.Length / 2 - 1;
+
+            points[midPoint].xCoordinate = points[midPoint].xCoordinate + 1;
+            points[midPoint].yCoordinate = points[midPoint].yCoordinate + 1;
+
+            SmallestDistanceBetweenPoints smallestDistanceBetweenPoints = new SmallestDistanceBetweenPoints();
+
+            int expectedOutput = 2;
+
+            // Act
+            int actualOutput = smallestDistanceBetweenPoints.FindSmallestDistanceBetweenPoints(points);
+
+            // Assert
+            Assert.Equal(expectedOutput, actualOutput);
+        }
+
+        [Fact]
+        public void OddNumberOfPoints_MinDistInTheBeginning()
+        {
+            // Arrange
+            Point[] points = new Point[101];
+
+            for (int i = 0; i < points.Length; i++)
+            {
+                points[i] = new Point(i * 2, i * 2);
+            }
+
+            points[0].xCoordinate = points[0].xCoordinate + 1;
+            points[0].yCoordinate = points[0].yCoordinate + 1;
+
+            SmallestDistanceBetweenPoints smallestDistanceBetweenPoints = new SmallestDistanceBetweenPoints();
+
+            int expectedOutput = 2;
+
+            // Act
+            int actualOutput = smallestDistanceBetweenPoints.FindSmallestDistanceBetweenPoints(points);
+
+            // Assert
+            Assert.Equal(expectedOutput, actualOutput);
+        }
+
+        [Fact]
+        public void OddNumberOfPoints_MinDistInTheSplit()
+        {
+            // Arrange
+            Point[] points = new Point[101];
+
+            for (int i = 0; i < points.Length; i++)
+            {
+                points[i] = new Point(i * 2, i * 2);
+            }
+
+            int midPoint = points.Length / 2 ;
+
+            points[midPoint].xCoordinate = points[midPoint].xCoordinate + 1;
+            points[midPoint].yCoordinate = points[midPoint].yCoordinate + 1;
+
+            SmallestDistanceBetweenPoints smallestDistanceBetweenPoints = new SmallestDistanceBetweenPoints();
+
+            int expectedOutput = 2;
+
+            // Act
+            int actualOutput = smallestDistanceBetweenPoints.FindSmallestDistanceBetweenPoints(points);
+
+            // Assert
+            Assert.Equal(expectedOutput, actualOutput);
+        }
+
+        [Fact]
+        public void PointsWithTheSameXValue()
+        {
+            // Arrange
+            Point[] points = new Point[101];
+
+            for (int i = 0; i < points.Length; i++)
+            {
+                points[i] = new Point(0, i * 2);
+            }
+
+            int midPoint = points.Length / 2;
+
+            points[midPoint].xCoordinate = points[midPoint].xCoordinate + 1;
+            points[midPoint].yCoordinate = points[midPoint].yCoordinate + 1;
+
+            SmallestDistanceBetweenPoints smallestDistanceBetweenPoints = new SmallestDistanceBetweenPoints();
+
+            int expectedOutput = 2;
+
+            // Act
+            int actualOutput = smallestDistanceBetweenPoints.FindSmallestDistanceBetweenPoints(points);
+
+            // Assert
+            Assert.Equal(expectedOutput, actualOutput);
+        }
+
+        [Fact]
+        public void PointsWithTheSameYValue()
+        {
+            // Arrange
+            Point[] points = new Point[101];
+
+            for (int i = 0; i < points.Length; i++)
+            {
+                points[i] = new Point(i * 2, 0);
+            }
+
+            int midPoint = points.Length / 2;
+
+            points[midPoint].xCoordinate = points[midPoint].xCoordinate + 1;
+            points[midPoint].yCoordinate = points[midPoint].yCoordinate + 1;
+
+            SmallestDistanceBetweenPoints smallestDistanceBetweenPoints = new SmallestDistanceBetweenPoints();
+
+            int expectedOutput = 2;
 
             // Act
             int actualOutput = smallestDistanceBetweenPoints.FindSmallestDistanceBetweenPoints(points);
