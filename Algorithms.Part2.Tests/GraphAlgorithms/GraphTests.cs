3f9e6dc49beabc58fde44e595194e1b184fd3d7e
ali elmas <a.ihsan.elmas@gmail.com>
2022-02-03 14:44:58 +0300

    start implementing a new graph representation

---
+++ Algorithms.Part2.Tests/GraphAlgorithms/GraphTests.cs
@@ -0,0 +1,64 @@
+﻿using Algorithms.Part2.GraphAlgorithms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Algorithms.Part2.Tests.GraphAlgorithms
+{
+    public class GraphTests
+    {
+        [Fact]
+        public void AddVertex_AddsOneVertex()
+        {
+            // Arrange
+            Graph graph = new Graph();
+            int expectedVertexIndex = 0;
+
+            // Act
+            graph.AddVertex();
+
+            // Assert
+            Assert.Equal(expectedVertexIndex, graph.VerticesId[0]);
+        }
+
+        [Fact]
+        public void AddVertex_AddsTwoVertices()
+        {
+            // Arrange
+            Graph graph = new Graph();
+            int expectedSecondVertexIndex = 1;
+
+            // Act
+            graph.AddVertex();
+            graph.AddVertex();
+
+            // Assert
+            Assert.Equal(expectedSecondVertexIndex, graph.VerticesId[1]);
+        }
+
+
+        // 0-1
+        [Fact]
+        public void AddEdge_AddsOneEdge()
+        {
+            // Arrange
+            Graph graph = new Graph();
+
+            // Act
+            graph.AddVertex();
+            graph.AddVertex();
+            graph.AddEdge(0, 1);
+            var connectedVerticesToVertex0 = graph.GetConnectedVertices(0);
+            var neighbourVertexIndex = 1;
+            var notNeighbourVertexIndex = 2;
+
+            // Assert
+            Assert.Contains(neighbourVertexIndex, connectedVerticesToVertex0);
+            Assert.DoesNotContain(notNeighbourVertexIndex, connectedVerticesToVertex0);
+        }
+
+    }
+}
---
+++ Algorithms.Part2/GraphAlgorithms/Graph.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Part2.GraphAlgorithms
+{
+    public class Graph
+    {
+        public List<int> VerticesId { get; private set; } = new List<int>();
+        public Dictionary<int, List<int>> VertexIdToConnectedVertexIds { get; private set; } =
+            new Dictionary<int, List<int>>();
+
+        int nextVertexIndex = 0;
+
+        public void AddVertex()
+        {
+            VerticesId.Add(nextVertexIndex++);
+        }
+
+        public void AddEdge(int vertex1Id, int vertex2Id)
+        {
+            if (VerticesId.Contains(vertex1Id) == false ||
+                VerticesId.Contains(vertex2Id) == false)
+            {
+                throw new ArgumentException("Invalid vertex Id");
+            }
+
+            ConnectSourceVertexToTarget(vertex1Id, vertex2Id);
+            ConnectSourceVertexToTarget(vertex2Id, vertex1Id);
+        }
+
+        private void ConnectSourceVertexToTarget(int sourceVertex, int targetVertex)
+        {
+            if (VertexIdToConnectedVertexIds.TryGetValue(sourceVertex, out _) == false)
+            {
+                VertexIdToConnectedVertexIds.Add(sourceVertex, new List<int>());
+
+            }
+
+            VertexIdToConnectedVertexIds[sourceVertex].Add(targetVertex);
+        }
+
+        public List<int> GetConnectedVertices(int vertexId)
+        {
+            VertexIdToConnectedVertexIds.TryGetValue(vertexId, out List<int> connectedVertices);
+            return connectedVertices;
+        }
+    }
+}
