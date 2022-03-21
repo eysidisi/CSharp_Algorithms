97073127c2a579e8a69a09b103efac23180e4e8e
ali elmas <a.ihsan.elmas@gmail.com>
2022-03-21 18:48:23 +0300

    Finish coursera assignment about hash tables

---
+++ Algorithms.Part2.Tests/HashTableAlgorithms/TwoSumsProblemTests.cs
@@ -0,0 +1,64 @@
+﻿using Algorithms.Part2.HashTableAlgorithms;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Algorithms.Part2.Tests.HashTableAlgorithms
+{
+    public class TwoSumsProblemTests
+    {
+        private readonly ITestOutputHelper output;
+
+        public TwoSumsProblemTests(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
+
+        [Theory]
+        [InlineData(new int[] { 1 }, new bool[] { false, false }, new int[] { 1, 2 })]
+        [InlineData(new int[] { 1, 2, 3 }, new bool[] { true, true, true, false }, new int[] { 3, 4, 5, -1 })]
+        [InlineData(new int[] { 1, 2, 3, 1, 1, 0, -10 }, new bool[] { true, true, true, true, false, false }, new int[] { 2, 1, -10, -8, 15, -20 })]
+        public void CheckIfSumExists_ReturnsTrue(int[] arr, bool[] expectedResults, int[] sums)
+        {
+            TwoSumsProblem twoSumsProblem = new TwoSumsProblem(arr);
+
+            for (int i = 0; i < expectedResults.Length; i++)
+            {
+                int sum = sums[i];
+                bool actualResult = twoSumsProblem.CheckIfSumExists(sum);
+                Assert.Equal(expectedResults[i], actualResult);
+            }
+        }
+
+        [Fact(Skip = "Coursera Assignment")]
+        public void CourseraAssignment()
+        {
+            string inputFolderPath = Directory.GetCurrentDirectory() + @"\HashTableAlgorithms\InputFiles\Coursera.txt";
+
+            TwoSumsProblem twoSumsProblem = new TwoSumsProblem(inputFolderPath);
+
+            int numOfSums = 0;
+            object lockObj = new object();
+
+            Parallel.For(-10000, 10001, targetSum =>
+             {
+                 if (twoSumsProblem.CheckIfSumExists(targetSum))
+                 {
+                     lock (lockObj)
+                     {
+                         numOfSums++;
+                     }
+                 }
+             });
+
+
+            output.WriteLine(numOfSums.ToString());
+        }
+    }
+}
---
+++ Algorithms.Part2/HashTableAlgorithms/TwoSumsProblem.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Part2.HashTableAlgorithms
+{
+    public class TwoSumsProblem
+    {
+        Dictionary<long, int> arrElementsToFrequency = new Dictionary<long, int>();
+        long[] arr;
+        public TwoSumsProblem(int[] arr)
+        {
+            this.arr = new long[arr.Length];
+            Array.Copy(arr, this.arr, arr.Length);
+            
+            AddArrayElementsToTheDictionary();
+            Array.Sort(this.arr);
+        }
+
+        public TwoSumsProblem(string inputFolderPath)
+        {
+            ReadFile(inputFolderPath);
+            Array.Sort(arr);
+            AddArrayElementsToTheDictionary();
+        }
+
+        private void ReadFile(string inputFolderPath)
+        {
+            string[] lines = File.ReadAllLines(inputFolderPath);
+
+            arr = new long[lines.Length];
+
+            int index = 0;
+
+            foreach (string line in lines)
+            {
+                arr[index] = long.Parse(line);
+                index++;
+            }
+        }
+
+        public bool CheckIfSumExists(long sum)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                long num1 = arr[i];
+
+                if (num1 > sum) return false;
+
+                long requiredNum = sum - num1;
+
+                if (requiredNum == num1)
+                {
+                    if (arrElementsToFrequency.TryGetValue(requiredNum, out int freq))
+                    {
+                        if (freq >= 2)
+                        {
+                            return true;
+                        }
+                    }
+                }
+
+                else
+                {
+                    if (arrElementsToFrequency.TryGetValue(requiredNum, out int _))
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        private void AddArrayElementsToTheDictionary()
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                long element = arr[i];
+
+                if (arrElementsToFrequency.ContainsKey(element))
+                {
+                    arrElementsToFrequency[element]++;
+                }
+                else
+                {
+                    arrElementsToFrequency.Add(element, 1);
+                }
+            }
+        }
+    }
+}
