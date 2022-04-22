f72a5414ff2405d058612e6f160366fe92fb2d66
ali elmas <a.ihsan.elmas@gmail.com>
2022-04-22 17:54:01 +0300

    Add calculating weighted independent set algorithm

---
+++ Algorithms.Part3.Tests/DynamicProgramming/GraphTests.cs
@@ -0,0 +1,93 @@
+﻿using Algorithms.Part3.DynamicProgramming;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Algorithms.Part3.Tests.DynamicProgramming
+{
+    public class GraphTests
+    {
+
+        private readonly ITestOutputHelper log;
+
+        public GraphTests(ITestOutputHelper output)
+        {
+            this.log = output;
+        }
+
+        [Theory]
+        [InlineData(new int[] { 1, 5 }, 5, new int[] { 1 })]
+        [InlineData(new int[] { 5, 1 }, 5, new int[] { 0 })]
+        public void CalculateMaximumWeightedIndependentSet_TwoNodes(int[] nodes, long expectedResult, int[] expectedUsedNodeIds)
+        {
+            Graph graph = new Graph(nodes);
+
+            long actualResult = graph.CalculateMaximumWeightedIndependentSet();
+            List<int> actualUsedNodeIDs = graph.UsedNodeIds.ToList();
+
+            Assert.Equal(expectedResult, actualResult);
+            Assert.Equal(expectedUsedNodeIds.ToList(), actualUsedNodeIDs);
+        }
+
+        [Theory]
+        [InlineData(new int[] { 1, 5, 2 }, 5, new int[] { 1 })]
+        [InlineData(new int[] { 5, 3, 1 }, 6, new int[] { 2, 0 })]
+        public void CalculateMaximumWeightedIndependentSet_ThreeNodes(int[] nodes, long expectedResult, int[] expectedUsedNodeIds)
+        {
+            Graph graph = new Graph(nodes);
+
+            long actualResult = graph.CalculateMaximumWeightedIndependentSet();
+            List<int> actualUsedNodeIDs = graph.UsedNodeIds.ToList();
+
+            Assert.Equal(expectedResult, actualResult);
+            Assert.Equal(expectedUsedNodeIds.ToList(), actualUsedNodeIDs);
+        }
+
+        [Theory]
+        [InlineData(new int[] { 1, 5, 2, 4 }, 9, new int[] { 3, 1 })]
+        [InlineData(new int[] { 5, 3, 4, 1 }, 9, new int[] { 2, 0 })]
+        [InlineData(new int[] { 5, 3, 1, 4 }, 9, new int[] { 3, 0 })]
+        public void CalculateMaximumWeightedIndependentSet_FourNodes(int[] nodes, long expectedResult, int[] expectedUsedNodeIds)
+        {
+            Graph graph = new Graph(nodes);
+
+            long actualResult = graph.CalculateMaximumWeightedIndependentSet();
+            List<int> actualUsedNodeIDs = graph.UsedNodeIds.ToList();
+
+            Assert.Equal(expectedResult, actualResult);
+            Assert.Equal(expectedUsedNodeIds.ToList(), actualUsedNodeIDs);
+        }
+        [Fact]
+        public void CalculateMaximumWeightedIndependentSet_CourseraAssignment()
+        {
+            string filePath = Directory.GetCurrentDirectory() + @"/DynamicProgramming/InputFiles/mwis.txt";
+
+            Graph graph = new Graph(filePath);
+
+            graph.CalculateMaximumWeightedIndependentSet();
+
+            List<int> nodeIdsToCheck = new List<int>() { 0, 1, 2, 3, 16, 116, 516, 996 };
+
+            string output="";
+
+            foreach (var nodeId in nodeIdsToCheck)
+            {
+                if (graph.UsedNodeIds.Contains(nodeId))
+                {
+                    output += "1";
+                }
+                else
+                {
+                    output += "0";
+                }
+            }
+
+            log.WriteLine(output);
+        }
+    }
+}
---
+++ Algorithms.Part3/DynamicProgramming/Graph.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Part3.DynamicProgramming
+{
+    public class Graph
+    {
+        public IReadOnlyCollection<int> UsedNodeIds => usedNodeIds;
+
+        int[] nodes;
+        long[] results;
+        List<int> usedNodeIds = new List<int>();
+
+        public Graph(int[] nodes)
+        {
+            this.nodes = nodes;
+        }
+
+        public Graph(string filePath)
+        {
+            string[] lines = File.ReadAllLines(filePath);
+
+            int numberOfNodes = int.Parse(lines[0]);
+
+            nodes = new int[numberOfNodes];
+
+            for (int i = 1; i < lines.Length; i++)
+            {
+                int weigth = int.Parse(lines[i]);
+                nodes[i - 1] = weigth;
+            }
+        }
+
+        public long CalculateMaximumWeightedIndependentSet()
+        {
+            results = new long[nodes.Length + 1];
+            results[0] = 0;
+            results[1] = nodes[0];
+
+            long result = CalculateMaximumWeightedIndependentSet(2);
+
+            CalculateUsedNodeIDs();
+
+            return result;
+        }
+
+        private void CalculateUsedNodeIDs()
+        {
+            int nodeIndex = nodes.Length - 1;
+
+            while (nodeIndex >= 1)
+            {
+                if (nodes[nodeIndex] + results[nodeIndex - 1] > results[nodeIndex])
+                {
+                    usedNodeIds.Add(nodeIndex);
+                    nodeIndex -= 2;
+                }
+                else
+                {
+                    nodeIndex--;
+                }
+            }
+
+            if (nodeIndex == 0)
+            {
+                usedNodeIds.Add(nodeIndex);
+            }
+        }
+
+        private long CalculateMaximumWeightedIndependentSet(int nodeNumberToCalculate)
+        {
+            if (nodeNumberToCalculate == nodes.Length)
+            {
+                long withLastNode = results[nodeNumberToCalculate - 2] + nodes[nodeNumberToCalculate - 1];
+                long withOutLastNode = results[nodeNumberToCalculate - 1];
+                return Math.Max(withLastNode, withOutLastNode);
+            }
+
+            else
+            {
+                long withThisNode = results[nodeNumberToCalculate - 2] + nodes[nodeNumberToCalculate - 1];
+                long withOutThisNode = results[nodeNumberToCalculate - 1];
+                results[nodeNumberToCalculate] = withThisNode > withOutThisNode ? withThisNode : withOutThisNode;
+                return CalculateMaximumWeightedIndependentSet(nodeNumberToCalculate + 1);
+            }
+        }
+    }
+}
